ee9b4891c31377575864dcf5a7318e97a4a25514
Armin Ronacher <armin.ronacher@active-4.com>
2020-01-22 23:16:46 +0100

    feat: Initial version code

--- src/parser.rs
+++ src/parser.rs
@@ -1,25 +1,88 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-
 lazy_static! {
     static ref RELEASE_REGEX: Regex = Regex::new(r#"^(@?[^@]+)@(.*?)$"#).unwrap();
-    static ref SEMVER_REGEX: Regex = Regex::new(r#"(?x)
+    static ref VERSION_REGEX: Regex = Regex::new(
+        r#"(?x)
         ^
-            (?P<major>0|[1-9][0-9]*)\.
-            (?P<minor>0|[1-9][0-9]*)\.
-            (?P<patch>0|[1-9][0-9]*)
-            (?:-(?P<prerelease>(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)
-            (?:\.(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?
-            (?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?
+            (?P<major>0|[1-9][0-9]*)
+            (?:\.(?P<minor>0|[1-9][0-9]*))?
+            (?:\.(?P<patch>0|[1-9][0-9]*))?
+            (?:-?
+                (?P<prerelease>(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)
+                (?:\.(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?
+            (?:\+(?P<build_code>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?
         $
-    "#).unwrap();
+    "#
+    )
+    .unwrap();
+}
+
+pub struct Version<'a> {
+    major: u64,
+    minor: u64,
+    patch: u64,
+    pre: &'a str,
+    build_code: &'a str,
+}
+
+impl<'a> Version<'a> {
+    pub fn new(version: &'a str) -> Option<Version<'a>> {
+        let caps = if let Some(caps) = VERSION_REGEX.captures(version) {
+            caps
+        } else {
+            return None;
+        };
+        Some(Version {
+            major: caps[1].parse().unwrap(),
+            minor: caps
+                .get(2)
+                .and_then(|x| x.as_str().parse().ok())
+                .unwrap_or(0),
+            patch: caps
+                .get(3)
+                .and_then(|x| x.as_str().parse().ok())
+                .unwrap_or(0),
+            pre: caps.get(4).map(|x| x.as_str()).unwrap_or(""),
+            build_code: caps.get(5).map(|x| x.as_str()).unwrap_or(""),
+        })
+    }
+
+    pub fn major(&self) -> u64 {
+        self.major
+    }
+
+    pub fn minor(&self) -> u64 {
+        self.minor
+    }
+
+    pub fn patch(&self) -> u64 {
+        self.patch
+    }
+
+    pub fn pre(&self) -> Option<&str> {
+        if self.pre.is_empty() {
+            None
+        } else {
+            Some(self.pre)
+        }
+    }
+
+    pub fn build_code(&self) -> Option<&str> {
+        if self.build_code.is_empty() {
+            None
+        } else {
+            Some(self.build_code)
+        }
+    }
 }
 
 pub struct Release<'a> {
     raw: &'a str,
     package: &'a str,
     version_raw: &'a str,
+    version: Option<Version<'a>>,
 }
 
 impl<'a> Release<'a> {
@@ -29,12 +92,14 @@ impl<'a> Release<'a> {
                 raw: release,
                 package: caps.get(1).unwrap().as_str(),
                 version_raw: caps.get(2).unwrap().as_str(),
+                version: Version::new(caps.get(2).unwrap().as_str()),
             }
         } else {
             Release {
                 raw: release,
                 package: "",
                 version_raw: release,
+                version: None,
             }
         }
     }
@@ -49,5 +114,9 @@ impl<'a> Release<'a> {
 
     pub fn version_raw(&self) -> &str {
         self.version_raw
+    }
+
+    pub fn version(&self) -> Option<&Version<'a>> {
+        self.version.as_ref()
     }
 }
--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -2,7 +2,28 @@ use sentry_release_parser::Release;
 
 #[test]
 fn test_basic() {
-    let release = Release::new("@foo.bar.baz/blah@1.2.3-dev");
+    let release = Release::new("@foo.bar.baz/blah@1.2.3-dev+build-code");
     assert_eq!(release.package(), "@foo.bar.baz/blah");
-    assert_eq!(release.version_raw(), "1.2.3-dev");
+    assert_eq!(release.version_raw(), "1.2.3-dev+build-code");
+
+    let version = release.version().unwrap();
+    assert_eq!(version.major(), 1);
+    assert_eq!(version.minor(), 2);
+    assert_eq!(version.patch(), 3);
+    assert_eq!(version.pre(), Some("dev"));
+    assert_eq!(version.build_code(), Some("build-code"));
+}
+
+#[test]
+fn test_basic_short_ver() {
+    let release = Release::new("@foo.bar.baz/blah@1a+build-code");
+    assert_eq!(release.package(), "@foo.bar.baz/blah");
+    assert_eq!(release.version_raw(), "1a+build-code");
+
+    let version = release.version().unwrap();
+    assert_eq!(version.major(), 1);
+    assert_eq!(version.minor(), 0);
+    assert_eq!(version.patch(), 0);
+    assert_eq!(version.pre(), Some("a"));
+    assert_eq!(version.build_code(), Some("build-code"));
 }
