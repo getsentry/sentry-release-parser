0b8a48a218292ca2a112e572f6c0c362f6d6ae25
Armin Ronacher <armin.ronacher@active-4.com>
2020-01-23 15:23:50 +0100

    doc: Added example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 //! This package implements a release name parser that is used by Sentry.
 //!
+//! ```
+//! let release = Release::parse("org.example.FooApp@1.0rc1+20200101100").unwrap();
+//! assert_eq!(release.package(), Some("org.example.FooApp"));
+//! assert_eq!(release.version_raw(), "1.0rc1+20200101100");
+//!
+//! let version = release.version().unwrap();
+//! assert_eq!(version.major(), 1);
+//! assert_eq!(version.minor(), 0);
+//! assert_eq!(version.patch(), 0);
+//! assert_eq!(version.triple(), (1, 0, 0));
+//! assert_eq!(version.pre(), Some("rc1"));
+//! assert_eq!(version.build_code(), Some("20200101100"));
+//! ```
+//!
 //! # Features
 //!
 //! - `semver`: if enabled the version object provides a method to convert it
