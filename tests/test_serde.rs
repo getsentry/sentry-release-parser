0d7deb0726c13392d7d9cef00e85082f4a366e7a
Armin Ronacher <armin.ronacher@active-4.com>
2021-01-02 01:11:53 +0100

    ref: Make release snapshots use syntax insta plugin can pick up

--- tests/test_serde.rs
+++ tests/test_serde.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "serde")]
 use sentry_release_parser::Release;
 
-macro_rules! release_snapshot {
+macro_rules! assert_release_snapshot {
     ($release:expr) => {
         fn release(s: &str) -> Release<'_> {
             Release::parse(s).unwrap()
@@ -12,60 +12,60 @@ macro_rules! release_snapshot {
 
 #[test]
 fn test_basic() {
-    release_snapshot!("@foo.bar.baz--blah@1.2.3-dev+BUILD-code");
+    assert_release_snapshot!("@foo.bar.baz--blah@1.2.3-dev+BUILD-code");
 }
 
 #[test]
 fn test_mobile() {
-    release_snapshot!("foo.bar.baz.App@1.0+20200101100");
+    assert_release_snapshot!("foo.bar.baz.App@1.0+20200101100");
 }
 
 #[test]
 fn test_mobile_three_components() {
-    release_snapshot!("foo.bar.baz.App@1.0.0+20200101100");
+    assert_release_snapshot!("foo.bar.baz.App@1.0.0+20200101100");
 }
 
 #[test]
 fn test_mobile_dotted_secondary() {
-    release_snapshot!("foo.bar.baz.App@1.0+1.0.200");
+    assert_release_snapshot!("foo.bar.baz.App@1.0+1.0.200");
 }
 
 #[test]
 fn test_hash() {
-    release_snapshot!("085240e737828d8326719bf97730188e927e49ca");
+    assert_release_snapshot!("085240e737828d8326719bf97730188e927e49ca");
 }
 
 #[test]
 fn test_qualified_hash() {
-    release_snapshot!("package@085240e737828d8326719bf97730188e927e49ca");
+    assert_release_snapshot!("package@085240e737828d8326719bf97730188e927e49ca");
 }
 
 #[test]
 fn test_single_component() {
-    release_snapshot!("com.foogame.FooGame@7211+7211");
+    assert_release_snapshot!("com.foogame.FooGame@7211+7211");
 }
 
 #[test]
 fn test_invalid_date_release() {
-    release_snapshot!("some-api@2020.05.26-01.38.42");
+    assert_release_snapshot!("some-api@2020.05.26-01.38.42");
 }
 
 #[test]
 fn test_valid_dotted_release() {
-    release_snapshot!("some-api@2020.2-1.2.3");
+    assert_release_snapshot!("some-api@2020.2-1.2.3");
 }
 
 #[test]
 fn test_basic_prerelease() {
-    release_snapshot!("some-api@1.2.3-test");
+    assert_release_snapshot!("some-api@1.2.3-test");
 }
 
 #[test]
 fn test_implied_prerelease() {
-    release_snapshot!("some-api@1.0alpha2");
+    assert_release_snapshot!("some-api@1.0alpha2");
 }
 
 #[test]
 fn test_dashed_numeric_prerelease() {
-    release_snapshot!("some-api@1.0-1234");
+    assert_release_snapshot!("some-api@1.0-1234");
 }
